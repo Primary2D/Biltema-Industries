48168eb232759411a3685f32f60afa01681cab37
Jimmie <tim.nilsson@elev.ga.lbs.se>
2017-01-20 10:02:14 +0100

    inget
    körv

---
+++ helloplanet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloPlanet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            helloworld HelloWorld;
+            HelloWorld = new helloworld();
+            HelloWorld.Run();
+
+        }
+    }
+}
---
+++ helloplanet/helloworld.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloPlanet
+{
+    class helloworld
+    {
+
+        public void Run()
+        {
+
+            Console.WriteLine("Hello World!");
+
+        }
+
+    }
+}
